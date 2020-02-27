7c8c31028b689623a48d0f5466233e73e1797cf4
Manish21-v <vitkarmanish21@gmail.com>
2020-02-27 15:46:22 +0530

    added library system assignment

---
+++ librarySystem.rb
@@ -0,0 +1,79 @@
+class Book
+  #getter setter for all the properties of Book 
+    attr_accessor :bookname ,:bookid, :issuedate , :returningdate
+  
+    #Array of class book to keep the record of the books available
+     @@bookarray = []
+    
+    def initialize
+      @bookname
+      @bookid
+      @issuedate
+      @returningdate
+      
+    end
+
+    #Shows all the available books in the library
+    def getdetail
+      @@bookarray.each do |item|
+      puts " bookname : #{item.bookid} \n book_id : #{item.bookname}"
+      end
+    end
+
+    #adds the new book in the library
+    def addbook
+      @@bookarray.push(self)
+    end
+
+end
+bobj=Book.new()
+bobj.bookid = 1
+bobj.bookname = "Mahabharat"
+bobj.addbook
+bobj2=Book.new()
+bobj2.bookid = 2
+bobj2.bookname = "Ramayan"
+bobj2.addbook
+bobj.getdetail
+
+
+
+
+class Member
+  #getter setter for the properties of the Member class 
+    attr_accessor :id ,:name ,:gender, :no_of_books
+        
+    def initialize(id, name, gender)
+        @id = id
+        @name = name
+        @gender = gender
+        @no_of_books = [] 
+    end
+
+    #function to borrow books
+    def borrower(bookid,bookname,issuedate,returningdate)
+      books=Book.new
+      books.bookid = bookid
+      books.bookname = bookname
+      books.issuedate = issuedate
+      books.returningdate = returningdate
+      puts no_of_books.class
+      self.no_of_books.push(books)
+    end
+    
+    #shows  the books borrowed by the member
+    def getdetail
+      $i=0
+      no_of_books.each do|item|
+      puts "Book id: #{item.bookid} \n Book name : #{item.bookname} \n issue date : #{item.issuedate} \n return date : #{item.returningdate}"
+      end
+    end
+
+end
+
+mobj=Member.new(100, 'ramesh', 'male')
+mobj.borrower(1,"Mahabharat","21/02/2020","21/03/2020")
+mobj.getdetail()
+mobj.borrower(2,"Ramayan","22/02/2020","22/03/2020")
+mobj.getdetail()
+
